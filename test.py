f1d2867cbdfd9d4094b174dede0e33e2d01cbf14
yj7o5 <yawarjamal@gmail.com>
2021-08-19 07:38:31 -0500

    add github actions to run tests

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import unittest
+
+def TestMapReduce(unittest.TestCase):
+
+    def test_output(self):
+        lines = open("output.txt", "r").readlines()
+        passed = any("PASSED ALL TESTS" in line for line in lines)
+
+        self.assertEqual(passed, True)
+
+if __name__ == '__main__':
+    unittest.main()
